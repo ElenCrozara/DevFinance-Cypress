9f6b1d9b1e37fa2d716a4134e498e11e7426f71a
elen <elen.crozzara@gmail.com>
2024-02-12 21:59:41 -0300

    Cadastrando saída e validando total

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -4,9 +4,29 @@ describe('Transações', () => {
         cy.visit('https://dev-finance.netlify.app/');
     });
     it('Cadastrando uma entrada', () => {
-        cy.contains("Nova Transação").click();
-        cy.get('#form').should('be.visible');
-        cy.get('#description').type('salario');
-        cy.get('#amount').type(800);
+        criarTransacao('salario', 800)
+        cy.get('tbody tr td.description').should('have.text', 'salario')
     });
+
+    it('Cadastrando uma saída', () => {
+        criarTransacao('cineminha', -75)
+        cy.get('tbody tr td.description').should('have.text', 'cineminha')
+    });
+
+    it('Validando o total', () => {
+        criarTransacao('comissão', 550)
+        criarTransacao('supermercado', -95)
+        cy.get('.total').to.eq(455)
+    });
+    
+    function criarTransacao(descricao,valor) {
+        cy.contains("Nova Transação").click();
+            cy.get('#form').should('be.visible');
+            cy.get('#description').type(descricao);
+            cy.get('#amount').type(valor);
+            cy.get('#date').type('2024-02-10') // yyyy-mm-dd
+            cy.get('button').click()
+    }
+
+    
 });
