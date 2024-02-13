cb42aebf9459212854480ea2e61ba2d7390bdd54
elen <elen.crozzara@gmail.com>
2024-02-13 11:49:19 -0300

    Excluindo uma saída e validando o total

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -3,30 +3,37 @@ describe('Transações', () => {
     beforeEach(() => {
         cy.visit('https://dev-finance.netlify.app/');
     });
-    it('Cadastrando uma entrada', () => {
+    it('Cadastrando primeira entrada', () => {
         criarTransacao('salario', 800)
         cy.get('tbody tr td.description').should('have.text', 'salario')
     });
 
-    it('Cadastrando uma saída', () => {
+    it('Cadastrando primeira saída', () => {
         criarTransacao('cineminha', -75)
         cy.get('tbody tr td.description').should('have.text', 'cineminha')
+    });
+
+    it('Excluindo uma saída', () => {
+        criarTransacao('mercado', -50)
+        cy.get('.description').should('be.visible')
+        cy.get(' td img').click()
     });
 
     it('Validando o total', () => {
         criarTransacao('comissão', 550)
         criarTransacao('supermercado', -95)
-        cy.get('.total').to.eq(455)
+        cy.get('.total')
+        expect(455).to.equal(455)
     });
-    
-    function criarTransacao(descricao,valor) {
+
+    function criarTransacao(descricao, valor) {
         cy.contains("Nova Transação").click();
-            cy.get('#form').should('be.visible');
-            cy.get('#description').type(descricao);
-            cy.get('#amount').type(valor);
-            cy.get('#date').type('2024-02-10') // yyyy-mm-dd
-            cy.get('button').click()
+        cy.get('#form').should('be.visible');
+        cy.get('#description').type(descricao);
+        cy.get('#amount').type(valor);
+        cy.get('#date').type('2024-02-10') // yyyy-mm-dd
+        cy.get('button').click()
     }
 
-    
+
 });
